7a706a2748acf6d72d2df66d2cd3df6deb978146
Agata Ars <agata.arseniuk01@gmail.com>
2023-05-06 16:24:23 +0200

    CounterFix

--- src/components_/counter/Counter.jsx
+++ src/components_/counter/Counter.jsx
@@ -15,31 +15,28 @@ export const Counter = ({ time }) => {
   }, []);
 
 
-  //TODO timer 
+  
+  let seconds = Math.floor(timer / 1000),
+     minutes = Math.floor(seconds / 60),
+     hours = Math.floor(minutes / 60),
+     days = Math.floor(hours / 24),
+     months = Math.floor(days / 30);
 
-  // console.log("timer", timer);
-  //
-  // let seconds = Math.floor(timer / 1000),
-  //   minutes = Math.floor(seconds / 60),
-  //   hours = Math.floor(minutes / 60),
-  //   days = Math.floor(hours / 24),
-  //   months = Math.floor(days / 30),
-  //
-  // seconds %= 60;
-  // minutes %= 60;
-  // hours %= 24;
-  // days %= 30;
-  // months %= 12;
+   seconds %= 60;
+   minutes %= 60;
+   hours %= 24;
+   days %= 30;
+   months %= 12;
 
   return (
     <div>
-      <p>{timer}</p>
-      {/*<p>seconds: {seconds}</p>*/}
-      {/*<p>minutes: {minutes}</p>*/}
-      {/*<p>hours: {hours}</p>*/}
-      {/*<p>days: {days}</p>*/}
-      {/*<p>months: {months}</p>*/}
-      {/*<p>years: {years}</p>*/}
+    
+      <p>seconds: {seconds}</p>
+      <p>minutes: {minutes}</p>
+      <p>hours: {hours}</p>
+      <p>days: {days}</p>
+      <p>months: {months}</p>
+      
     </div>
   );
 };
